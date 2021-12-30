c67a9855e9874b30555376a2c468da29a1f0ec31
Nikhil10062000 <94104092+Nikhil10062000@users.noreply.github.com>
2021-12-30 11:47:34 +0530

    Add files via upload

---
+++ ass202.js
@@ -0,0 +1,28 @@
+ //Check whether the given character is vowel or not
+
+
+
+var char =prompt("Enter char");
+if (char =="a" && "A")
+{
+alert("its a vowel");
+ }
+else if (char =="e" && "E")
+{
+alert("its a vowel");
+}
+else if(char =="i" && "I")
+{
+alert("its a vowel");
+}
+else if(char =="o" && "O")
+{
+alert("its a vowel");
+}
+else if(char =="u" && "U")
+{
+alert("its a vowel");
+} 
+else {
+alert ("its a constant");
+}
